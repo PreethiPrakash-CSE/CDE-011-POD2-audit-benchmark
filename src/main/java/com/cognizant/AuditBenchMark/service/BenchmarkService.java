511f22762487c79890c1114eed6afd34f7d395f8
DESKTOP-SJPSRJH <hp@DESKTOP-SJPSRJH.(none)>
2022-07-29 16:20:14 +0530

    first update to benchmark repo

---
+++ src/main/java/com/cognizant/AuditBenchMark/service/BenchmarkService.java
@@ -0,0 +1,12 @@
+package com.cognizant.AuditBenchMark.service;
+
+import java.util.List;
+
+import com.cognizant.AuditBenchMark.model.AuditBenchmark;
+
+public interface BenchmarkService {
+
+	public List<AuditBenchmark> auditBenchmark();
+	
+	
+}
